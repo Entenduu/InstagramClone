de3a56f24ce9f34263cef44fc70aee4158ecab74
Cristian <Entenduu@gmail.com>
2022-06-21 17:08:53 -0400

    bootstrapping shit

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -4,15 +4,17 @@ import Card from 'react-bootstrap/Card'
 function PostCard({post}) {
   return (
     <div>
+      <div class='card-container'>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={post.image}/>
-      <Card.Body>
-      <Card.Subtitle>{post.username}</Card.Subtitle>
-        <Card.Text>
-          {post.caption}
-        </Card.Text>
-      </Card.Body>
-</Card>
+        <Card.Img variant="top" src={post.image}/>
+        <Card.Body>
+        <Card.Subtitle>{post.username}</Card.Subtitle>
+          <Card.Text>
+            {post.caption}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
     </div>
   )
 }
