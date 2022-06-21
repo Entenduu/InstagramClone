4274106373f5567f15857f9feddd5d45c1468803
Cristian <Entenduu@gmail.com>
2022-06-21 16:10:36 -0400

    added instagram logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import {NavLink, Link} from 'react-router-dom'
 function NavBar() {
   return (
     <div>
+      <img src='./images/logo.png' alt='instagram logo'/>
         <NavLink to="/feed/explore" >Explore</NavLink>
         <NavLink to="/feed/profile" >Profile</NavLink>
         <Link exact to="/" >Log Out</Link>
