91aa3a62216581cde2453164b9db90c891788fce
Griffin Dooley <dooleygriffin@gmail.com>
2022-06-21 15:47:14 -0400

    added PostCard

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, Link} from 'react-router-dom'
 
 function NavBar() {
   return (
     <div>
         <NavLink to="/feed/explore" >Explore</NavLink>
         <NavLink to="/feed/profile" >Profile</NavLink>
+        <Link exact to="/" >Log Out</Link>
     </div>
   )
 }
