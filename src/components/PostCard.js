2e639f4b8c61f223f05e121cc24deed9168c15dc
Cristian <Entenduu@gmail.com>
2022-06-21 16:25:38 -0400

    bootstrap

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
 
 function PostCard({post}) {
   return (
     <div>
-      <img src={post.image} alt={post.caption}></img>
-      <p>{post.username}</p>
-      <p>{post.caption}</p>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={post.image}/>
+      <Card.Body>
+      <Card.Subtitle>{post.username}</Card.Subtitle>
+        <Card.Text>
+          {post.caption}
+        </Card.Text>
+      </Card.Body>
+</Card>
     </div>
   )
 }
