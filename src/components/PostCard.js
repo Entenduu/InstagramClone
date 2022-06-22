f67a1c7013fee4c7846fdc12e9cd6178026a163a
Cristian <Entenduu@gmail.com>
2022-06-22 12:00:24 -0400

    merge conflicts

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 
 function PostCard({post}) {
   return (
@@ -8,13 +9,20 @@ function PostCard({post}) {
       <br/>
       <br/>
       <div class='card-container'>
+
       <Card style={{ width: '28rem' }}>
+      <Card.Header>
+        <img src={post.profilePic} alt='pfp' className='img-fluid rounded-circle float-left' style={{ width: '2em', height: '2em', marginRight:'1em'}}></img>
+        <b>{post.username}</b>
+        </Card.Header>
         <Card.Img variant="top" src={post.image}/>
         <Card.Body>
-        <Card.Subtitle>{post.username}</Card.Subtitle>
+          <div>
+        <Card.Subtitle >liked by</Card.Subtitle>
           <Card.Text>
-            {post.caption}
+            <b>{post.username}</b>  {post.caption}
           </Card.Text>
+          </div>
         </Card.Body>
       </Card>
       </div>
