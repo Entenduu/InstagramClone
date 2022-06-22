c5bf04b9574982094a5990e3dc0b2ad1e0e53aa6
Cristian <Entenduu@gmail.com>
2022-06-22 18:02:16 -0400

    fixed feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,13 +3,8 @@ import NavBar from './NavBar'
 import {Route, Switch} from 'react-router-dom'
 import Liked from './Liked'
 import UserProfile from './UserProfile'
-<<<<<<< HEAD
 import AllPosts from './AllPosts'
-
-=======
-import PostCard from './PostCard'
 import MakeAPost from './MakeAPost'
->>>>>>> dcc490bd1786cdb38ae357a00acc3d6ee2788df2
 
 function Feed({user, postsData}) {
   
@@ -17,6 +12,7 @@ function Feed({user, postsData}) {
   return (
     <div>
         <NavBar /> 
+        
         <Switch>
             <Route path="/feed/liked" >
                 <Liked />
@@ -24,13 +20,10 @@ function Feed({user, postsData}) {
             <Route path="/feed/profile">
                 <UserProfile postsData={postsData} user={user}/>
             </Route>
-<<<<<<< HEAD
-            <AllPosts postsData={postsData}/>
-=======
             <Route path="/feed/makeapost">
                 <MakeAPost />
             </Route>
->>>>>>> dcc490bd1786cdb38ae357a00acc3d6ee2788df2
+            <AllPosts postsData={postsData}/>
         </Switch>
     </div>
   )
