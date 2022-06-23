cd3796ccc5d6a4e9cf3c26b1ba590f0c1cf0277f
Griffin Dooley <dooleygriffin@gmail.com>
2022-06-23 07:34:20 -0400

    make a post form is controlled

--- src/components/MakeAPost.js
+++ src/components/MakeAPost.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 function MakeAPost() {
+
+    const [formFields, setFormFields] = useState({image: '', caption: ''})
+
+    function handleOnChange(e) {
+        setFormFields(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+    console.log(formFields)
+
   return (
     <div>
-        <form>
+        <form onSubmit={e => handleSubmit(e)} >
             <h2>Make a Post</h2>
             <div className='form-group'>
               <label>Image URL:</label>
-              <input type="url"/>
+              <input onChange={e => setFormFields({...formFields, image: e.target.value})} value={formFields.image} type="url"/>
             </div>
             <div className='form-group'>
               <label>Caption:</label>
-              <input type="text"/>
+              <input onChange={e => setFormFields({...formFields, caption: e.target.value})} value={formFields.caption} type="text"/>
             </div>
             <input type="submit" value="Post" />
         </form>
